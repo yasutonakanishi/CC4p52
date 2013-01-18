b44daf953815de199e09171de1f92a52b1d84358
Atsushi Tadokoro <tadokoro@gmail.com>
2013-01-18 12:16:13 +0900

    Add WS06_ShadowProjectionTemplate

--- src/simulation/workshop/WS06_ShadowProjectionTemplate.java
+++ src/simulation/workshop/WS06_ShadowProjectionTemplate.java
@@ -1,23 +1,15 @@
-/*
- * プロジェクションマッピングのためのテンプレート
- *
- */
-
 package simulation.workshop;
 
-import net.unitedfield.cc.PAppletProjectorNode;
+import net.unitedfield.cc.PAppletProjectorShadowNode;
 import net.unitedfield.cc.util.SpatialInspector;
 import processing.core.PApplet;
-import simulation.p5.P_2_2_5_01;
-import simulation.p5.ParticleSphere;
-
+import simulation.p5.SimpleGridPApplet;
 import com.jme3.app.SimpleApplication;
 import com.jme3.light.AmbientLight;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
-import com.jme3.post.TextureProjectorRenderer;
 import com.jme3.renderer.queue.RenderQueue.ShadowMode;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
@@ -25,11 +17,11 @@ import com.jme3.scene.shape.Box;
 import com.jme3.texture.Texture;
 import com.jme3.util.SkyFactory;
 
-public class WS06_MultiProjection extends SimpleApplication {
-	PApplet applet1, applet2;
-	Spatial object;
+public class WS06_ShadowProjectionTemplate extends SimpleApplication {
+	Spatial object;	
+	PApplet applet;
 	int 	appletWidth, appletHeight;
-	PAppletProjectorNode projectorNode1, projectorNode2;
+	PAppletProjectorShadowNode projectorNode;
 	
 	@Override
 	// 初期設定
@@ -40,22 +32,18 @@ public class WS06_MultiProjection extends SimpleApplication {
 		setupProjector();
 		setupSpatialInspector();
 	}
-	
 	/******************* ここから下を設定 **************************/
 	
 	// 1. 投射するアプレットを選択
 	private void selectApplet(){
 		// Appletを選択
-		applet1 = new P_2_2_5_01();
+		// applet = new P_2_2_5_01();
 		// applet = new P_2_1_1_03();
 		// applet = new ColorBarsPApplet();
 		// applet = new CapturePApplet();
 		// applet = new GravitySim();
-		// applet = new HyperbolicCoral();
-		// applet = new MikuFlipPApplet();
-		// applet = new SimpleGridPApplet();
-		// applet = new P_2_1_1_03();
-		applet2 = new ParticleSphere();
+		applet = new SimpleGridPApplet();
+		// applet = new ParticleSphere();
 		
 		// 選択したアプレットの幅と高さを指定
 		appletWidth = 400;
@@ -79,32 +67,16 @@ public class WS06_MultiProjection extends SimpleApplication {
 	
 	// 3. プロジェクタ設定
 	private void setupProjector(){
-		// プロジェクタで投影する画像をレンダリングするTextureProjectorRendererを作成しviewPortに追加
-		TextureProjectorRenderer ptr = new TextureProjectorRenderer(assetManager);
-		viewPort.addProcessor(ptr);
+		// プロジェクターを作成
+		projectorNode = new PAppletProjectorShadowNode("projector0", assetManager, viewPort, 30, 200, 960, 240, applet, appletWidth, appletHeight, true);
 		
-		// 新プロジェクター(1)を作成
-		projectorNode1 = new PAppletProjectorNode("projector1", assetManager, applet1, appletWidth, appletHeight, true);
 		// 建物にあわせて、プロジェクタの配置する位置を決定
-		projectorNode1.setLocalTranslation(new Vector3f(0, 4f, 150));
+		projectorNode.setLocalTranslation(new Vector3f(0, 4f, 150));
 		// 建物にあわせて、プロジェクタの視点(つまり投射する位置)を決定
-		projectorNode1.lookAt(new Vector3f(0, 40f, 0f), Vector3f.UNIT_Y);
+		projectorNode.lookAt(new Vector3f(0, 40f, 0f), Vector3f.UNIT_Y);
 		// プロジェクタを追加
-		rootNode.attachChild(projectorNode1);
-		rootNode.attachChild(projectorNode1.getFrustmModel());
-		// プロジェクタをTextureProjectorRendereに追加
-		ptr.getTextureProjectors().add(projectorNode1.getProjector());
-		
-		// 新プロジェクター(2)を作成
-		projectorNode2 = new PAppletProjectorNode("projector2", assetManager, applet2, appletWidth, appletHeight, true);
-		// 建物にあわせて、プロジェクタの配置する位置を決定
-		projectorNode2.setLocalTranslation(new Vector3f(0, 4f, -150));
-		// 建物にあわせて、プロジェクタの視点(つまり投射する位置)を決定
-		projectorNode2.lookAt(new Vector3f(0, 40f, 0f), Vector3f.UNIT_Y);
-		// プロジェクタを追加
-		rootNode.attachChild(projectorNode2);
-		rootNode.attachChild(projectorNode2.getFrustmModel());
-		ptr.getTextureProjectors().add(projectorNode2.getProjector());
+		rootNode.attachChild(projectorNode);
+		rootNode.attachChild(projectorNode.getFrustmModel());
 	}
 	
 	/******************* ここから下は必要に応じて設定 **************************/
@@ -119,7 +91,7 @@ public class WS06_MultiProjection extends SimpleApplication {
 		
 		// ライティング
 		DirectionalLight sun = new DirectionalLight();
-		sun.setColor(ColorRGBA.White.mult(2.0f));
+		sun.setColor(ColorRGBA.White.mult(0.5f));
 		sun.setDirection(new Vector3f(0f, 1.0f, -0.75f));
 		rootNode.addLight(sun);
 		AmbientLight al = new AmbientLight();
@@ -162,8 +134,7 @@ public class WS06_MultiProjection extends SimpleApplication {
 	
 	private void setupSpatialInspector() {
 		SpatialInspector spatialInspector = SpatialInspector.getInstance();
-		projectorNode1.addControl(spatialInspector);
-		projectorNode2.addControl(spatialInspector);
+		projectorNode.addControl(spatialInspector);		
 		this.setPauseOnLostFocus(false);
 		spatialInspector.show();		
 	}
@@ -177,7 +148,7 @@ public class WS06_MultiProjection extends SimpleApplication {
 	
 	// メイン
 	public static void main(String[] args){
-		SimpleApplication app = new WS06_MultiProjection();
+		SimpleApplication app = new WS06_ShadowProjectionTemplate();
 		app.setPauseOnLostFocus(false); 
 		app.start();
 	}
